cd08118f8550fbcca7c96ea937d4dbe72c8ea0dc
Petru <p3tr1q@gmail.com>
2020-10-28 15:54:37 +0200

    implemented task

--- example/src/index.js
+++ example/src/index.js
@@ -1,11 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
-import { useIntl } from 'ebs-intl';
+import { LangProvider, useIntl } from 'ebs-intl';
 
-const App = () => {
-  const t = useIntl();
+const translate = {
+  greeting: {
+    en: 'Hello, {name} {surname}!',
+    ro: 'Buna, {name} {surname}!',
+  },
+  goodbye: {
+    en: 'Bye, {name}',
+    ru: 'Пока, {name}',
+  },
+  question: {
+    en: 'How are you, {name}?',
+    ru: 'Как дела, {name}?',
+    ro: 'Cum te simti, {name}?',
+  },
+};
 
-  return <div>{t('greeting')}</div>;
+const Info = () => {
+  const { t } = useIntl();
+
+  return (
+    <div>
+      <div>{t('greeting', { name: 'EBS', surname: 'Integrator' })}</div>
+      <div>{t('goodbye', { name: 'Coronavirus' })}</div>
+      <div>{t('question', { name: 'Chisinau' })}</div>
+    </div>
+  );
+};
+
+export const App = () => {
+  const [curentLang, setLang] = useState('en');
+
+  return (
+    <div>
+      <LangProvider translate={translate} locale={curentLang}>
+        <Info />
+      </LangProvider>
+      <div style={{ display: 'flex', marginTop: '.5rem' }}>
+        <div>
+          <button
+            style={{ marginRight: '1rem' }}
+            onClick={() => {
+              setLang('en');
+            }}
+          >
+            English
+          </button>
+        </div>
+        <div>
+          <button
+            style={{ marginRight: '1rem' }}
+            onClick={() => {
+              setLang('ro');
+            }}
+          >
+            Română
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setLang('ru');
+            }}
+          >
+            Русский
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 render(<App />, document.getElementById('root'));
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,69 @@
-export const useIntl = () => {
-  const intls = {
-    greeting: 'Hello, World!',
-  };
+import React, { createContext, ReactNode, useContext, useMemo } from 'react';
 
-  return (key: string): string => intls[key];
+const languages = ['en', 'ru', 'ro'];
+
+type Locale = 'en' | 'ru' | 'ro';
+
+type Props = {
+  translate: Translate;
+  locale: Locale;
+  children: ReactNode;
 };
+
+interface Translate {
+  [translationKey: string]: {
+    [language in Locale]: string;
+  };
+}
+
+interface TranslateUtils {
+  lang: Locale;
+  t: (key: string, params?: { [key: string]: string }) => string;
+}
+
+const LangContext = createContext<TranslateUtils | undefined>(undefined);
+
+function getTranslation(translate: Translate, locale: Locale, key: string, params: any) {
+  if (!translate?.[key]?.[locale]) {
+    return key.toUpperCase();
+  }
+
+  return translate[key][locale].replace(/{\w+}/g, (match) => {
+    const arg = match.replace(/\{|\}/g, '');
+
+    return params[arg] || match;
+  });
+}
+
+export const useTranslation = (locale: Locale, translate: Translate) =>
+  useMemo<TranslateUtils>(
+    () => ({
+      lang: locale,
+      t: (key, params) => getTranslation(translate, locale, key, params),
+    }),
+    [locale, translate],
+  );
+
+export function LangProvider(props: Props) {
+  const { children, translate, locale } = props;
+
+  Object.keys(translate).map((translationKey) => {
+    languages.map((lang) => {
+      if (!Object.prototype.hasOwnProperty.call(translate[translationKey], lang)) {
+        console.log(`Missing translation for '${translationKey}' with key '${lang}'`);
+      }
+    });
+  });
+
+  return <LangContext.Provider value={useTranslation(locale, translate)}>{children}</LangContext.Provider>;
+}
+
+export function useIntl(): TranslateUtils {
+  const context = useContext(LangContext);
+
+  if (!context) {
+    throw new Error('use useIntl inside LangProvider');
+  }
+
+  return context;
+}
